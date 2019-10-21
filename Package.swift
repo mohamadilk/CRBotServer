7863e32a6565aba4e866560cd46000f84ec3729f
Mohammad Ilkhani <>
2019-10-21 17:49:22 +0300

    -Prepare for development

--- Package.swift
+++ Package.swift
@@ -2,21 +2,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "CRBotServer",
-    products: [
-        .library(name: "CRBotServer", targets: ["App"]),
-    ],
+    name: "TILApp",
     dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-
-        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
-    ],
-    targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
+        .package(url: "https://github.com/vapor/vapor.git",
+                 from: "3.0.0"),
+        // 1
+        .package(
+            url: "https://github.com/vapor/fluent-postgresql.git",
+            from: "1.0.0"),
+    ], targets: [
+        // 2
+        .target(name: "App", dependencies: ["FluentPostgreSQL",
+                                            "Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App"])
+        .testTarget(name: "AppTests", dependencies: ["App"]),
     ]
 )
 
